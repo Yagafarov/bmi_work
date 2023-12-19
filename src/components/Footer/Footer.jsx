270261dfc47177ec1dde8f9828d8e929574f117c
Dinmuhammad Yagafarov <95093475+Yagafarov@users.noreply.github.com>
2023-12-19 07:27:33 +0000

    chnage navbar and footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
 import './Footer.css'
-import Logo from '../../assets/images/navbar/LogoLight.svg'
+import { Link } from 'react-router-dom';
+const { Footer } = Layout;
+import FooterLogo from '../../assets/images/navbar/LogoLight.svg'
 
-const Footer = () => {
-    return (
-        <footer className='footer'>
-            This is Footer
-        </footer>
-    )
-}
+const AppFooter = () => {
+  return (
+    <Footer className='footer ' >
+        <div className="container">
+      <Row justify="center">
+        <Col xs={24} sm={24} md={12} lg={12} xl={12} xxl={12}>
+          <h2 className='footer__logo' >
+            <img src={FooterLogo} alt="" />
+          </h2>
+          <p>
+            EduAdd tizimida siz eng zamonaviy o'quvv kurlarini mutlaqo bepulga ,<br/> mustaqil ravishta o'rganishingiz mumkin bo'ladi. 
+          </p>
+        </Col>
+        <Col xs={24} sm={24} md={6} lg={6} xl={6} xxl={6}>
+          <h3 className='footer__title' >Havolalar</h3>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact</li>
+            {/* Add more links as needed */}
+          </ul>
+        </Col>
+        <Col xs={24} sm={24} md={6} lg={6} xl={6} xxl={6}>
+          <h3 className='footer__title' >Bog'lanish</h3>
+          <p>
+            Manzil:<br />
+            Samarqand, O'zbekiston<br />
+            Email: info@eduadd.uz<br />
+            Telefon: +998 (99) 123 1212
+          </p>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+          <p style={{ textAlign: 'center', marginTop: '20px' }}>
+            © {new Date().getFullYear()} <Link to={"https://anodra.uz"}>wwww.anodra.uz</Link>  . Barcha huquqlar himoyalangan.
+          </p>
+        </Col>
+      </Row>
+      </div>
+    </Footer>
+  );
+};
 
-export default Footer
+export default AppFooter;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,15 +3,16 @@ import './Navbar.css'
 import Logo from '../../assets/images/navbar/Logo.svg'
 import { Link } from 'react-router-dom'
 import { Button } from 'antd'
-// import Button from '../Button/Button'
 
 
 const Navbar = () => {
+    
     return (
         <navbar className="navbar">
             <div className="container">
                 <div className="navbar__logo">
                     <img src={Logo} alt="" className="navbar__logo-img" />
+                    <div className="demo-logo" />
                 </div>
                 <ul className="navbar__menu">
                     <li className="navbar__menu-item">
@@ -34,10 +35,8 @@ const Navbar = () => {
                     </li>
                 </ul>
                 <div className="navbar__btns">
-                    {/* <Button content="Kirish" variant="link"/>
-                    <Button content="Ro'yxatdan o'tish" variant="default"/> */}
                     <Button type='link' size='large' >Kirish</Button>
-                    <Button type='primary' >Ro'yxatdan o'tish</Button>
+                    <Button type='primary' size='large' >Ro'yxatdan o'tish</Button>
                 </div>
             </div>
         </navbar>
