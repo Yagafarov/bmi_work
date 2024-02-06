21b5229ffcf92a32c70564f7a42ef7d325cad442
Dinmuhammad Yagafarov <95093475+Yagafarov@users.noreply.github.com>
2024-02-06 07:55:57 +0000

    changed  carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,59 +4,99 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-import './Carousel.css'
+import './Carousel.css';
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Parallax, Pagination, Navigation } from 'swiper/modules';
+import { Grid } from '@mui/material';
 
-export default function Carousel() {
+export default function App() {
   return (
     <>
       <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
         }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        speed={600}
+        parallax={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Parallax, Pagination, Navigation]}
         className="mySwiper"
       >
+        <div
+          slot="container-start"
+          className="parallax-bg"
+          data-swiper-parallax="-23%"
+        ></div>
         <SwiperSlide>
-          <img src="https://www.aisociety-unipd.it/wp-content/uploads/2021/04/Design-Thinking-1-uai-1081x1081.jpeg" />
+          
+          <Grid container >
+            <Grid item xs={12} md={6} ><div>
+              <div className="title" data-swiper-parallax="-300">
+                Slide 1
+              </div>
+              <div className="subtitle" data-swiper-parallax="-200">
+                Subtitle
+              </div>
+              <div className="text" data-swiper-parallax="-100">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                  dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+                  laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+                  Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+                  Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+                  ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+                  tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+                </p>
+              </div>
+            </div></Grid>
+            <Grid item xs={12} md={6} ><img src="https://img.freepik.com/free-photo/young-handsome-man-holding-notebooks-concept-e-learning-courses_1258-26588.jpg" style={{ borderRadius: "20px" }} /></Grid>
+          </Grid>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          <div className="title" data-swiper-parallax="-300">
+            Slide 2
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+          <div className="title" data-swiper-parallax="-300">
+            Slide 3
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
         </SwiperSlide>
       </Swiper>
     </>
