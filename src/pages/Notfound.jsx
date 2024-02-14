487e076ad000aa7c58b55e0b80c85ae966687153
Dinmuhammad Yagafarov <95093475+Yagafarov@users.noreply.github.com>
2024-02-14 03:24:16 +0000

    edit not found page

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,6 +1,6 @@
 import { Box, Button, Container, Grid, ThemeProvider, Typography, createTheme, responsiveFontSizes } from '@mui/material'
 import React from 'react'
-
+import RotateLeft from '@mui/icons-material/RotateLeft';
 const Notfound = () => {
 
   let theme = createTheme();
@@ -8,21 +8,21 @@ const Notfound = () => {
 
 
   return (
-    <Box style={{ minHeight: '90vh', marginTop: '70px' }} overflow="hidden" mt={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }} display={'flex'} alignItems={'center'} >
+    <Box style={{ minHeight: '40vh', marginTop: '70px' }} overflow="hidden" mb={{ xs: 2, sm: 3, md: 4, lg: 5, xl: 6 }} display={'flex'} alignItems={'center'} >
       <Container maxWidth="xl" display={'flex'} alignItems={'center'}>
-        <Grid container display={'flex'} alignItems={'center'} justifyContent={'space-around'}  >
-          <Grid item xs={12} md={4} height={'100%'} alignItems={'center'}>
-            <img src="https://i.pinimg.com/originals/a8/12/1a/a8121abee959e18cbad25ad4046f76d8.gif" width={'100%'} alt="" style={{ maxWidth: '150%', height: 'auto' }} />
-          </Grid>
-          <Grid item xs={12} md={4} textAlign={{xs:'center',md:'left'}} >
+        <Box  display='flex' alignItems={'center'} justifyContent={'center'} >
+          <Box position={'relative'} >
+            <img width={'90%'} src="https://miro.medium.com/v2/resize:fit:1400/1*zE2qnVTJehut7B8P2aMn3A.gif" alt="" zIndex={1} />
+            <Box position={'absolute'} zIndex={2} bottom={0} left={'0%'} right={'0%'} textAlign={'center'} >
             <ThemeProvider theme={theme}  >
               <Typography variant='h3' fontWeight={'bold'} mb={3}>
                 Joriy sahifa topilmadi.
               </Typography>
-              <Button variant='contained' style={{ background: 'var(--green)' }} >Qayta yuklash</Button>
+              <Button variant='contained' style={{ background: 'var(--green)' }} startIcon={<RotateLeft/>} >Qayta yuklash</Button>
             </ThemeProvider>
-          </Grid>
-        </Grid>
+            </Box>
+          </Box>
+        </Box>
       </Container>
     </Box>
   )
